5260b4f19495c04ce720a35eb167bb3dfd1926c9
yoliyko <59864721+keeeeonix@users.noreply.github.com>
2024-04-13 14:28:15 +0300

    Add files via upload

---
+++ OOPlab18/OOPlab18/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] array = { -2, 0, 3, -4, 5, 0, -7 }; // Приклад масиву (можна змінити)
+
+        Console.WriteLine("Початковий масив:");
+        PrintArray(array);
+
+        double maxAbsElement = CalculateMaxAbsoluteElement(array);
+        Console.WriteLine($"Максимальний за модулем елемент масиву: {maxAbsElement}");
+
+        double sumBetweenPositive = CalculateSumBetweenPositiveElements(array);
+        Console.WriteLine($"Сума елементів масиву, розташованих між першим і другим додатними: {sumBetweenPositive}");
+
+        TransformArray(array);
+
+        Console.WriteLine("Масив після перетворення:");
+        PrintArray(array);
+    }
+
+    static double CalculateMaxAbsoluteElement(double[] array)
+    {
+        double maxAbsElement = Math.Abs(array[0]);
+        for (int i = 1; i < array.Length; i++)
+        {
+            double absElement = Math.Abs(array[i]);
+            if (absElement > maxAbsElement)
+            {
+                maxAbsElement = absElement;
+            }
+        }
+        return maxAbsElement;
+    }
+
+    static double CalculateSumBetweenPositiveElements(double[] array)
+    {
+        int firstPositiveIndex = -1;
+        int secondPositiveIndex = -1;
+
+        // Знаходимо індекси першого та другого додатних елементів
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > 0)
+            {
+                if (firstPositiveIndex == -1)
+                {
+                    firstPositiveIndex = i;
+                }
+                else
+                {
+                    secondPositiveIndex = i;
+                    break;
+                }
+            }
+        }
+
+        // Перевірка чи знайдено обидва додатні елементи
+        if (firstPositiveIndex == -1 || secondPositiveIndex == -1)
+        {
+            Console.WriteLine("Не знайдено два додатніх елементи для обчислення суми між ними.");
+            return 0;
+        }
+
+        // Обчислення суми елементів масиву між першим і другим додатними
+        double sum = 0;
+        for (int i = firstPositiveIndex + 1; i < secondPositiveIndex; i++)
+        {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    static void TransformArray(double[] array)
+    {
+        // Створюємо новий масив, в якому нулі розміщуються в кінці
+        double[] transformedArray = array.Where(x => x != 0).Concat(array.Where(x => x == 0)).ToArray();
+
+        // Копіюємо елементи з transformedArray назад в оригінальний масив
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = transformedArray[i];
+        }
+    }
+
+    static void PrintArray(double[] array)
+    {
+        foreach (var element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ OOPlab18_2/OOPlab18_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = {
+            { 1, 2, 3, 4, 5 },
+            { 6, 7, 8, 9, 10 },
+            { 11, 12, 13, 14, 15 },
+            { 16, 17, 18, 19, 20 },
+            { 21, 22, 23, 24, 25 }
+        }; // Приклад двовимірного масиву 5x5
+
+        Console.WriteLine("Двовимірний масив:");
+        PrintArray(array);
+
+        int sumOfTwoElements = CalculateSumOfTwoElementsInSecondRow(array, 0, 2); // Приклад розрахунку суми двох будь-яких елементів другого рядка
+        Console.WriteLine($"Сума двох елементів другого рядка: {sumOfTwoElements}");
+
+        int productOfTwoElements = CalculateProductOfTwoElementsInFifthColumn(array, 1, 2); // Приклад розрахунку добутку двох будь-яких елементів п’ятого стовбця
+        Console.WriteLine($"Добуток двох елементів п'ятого стовбця: {productOfTwoElements}");
+    }
+
+    static int CalculateSumOfTwoElementsInSecondRow(int[,] array, int columnIndex1, int columnIndex2)
+    {
+        int sum = array[1, columnIndex1] + array[1, columnIndex2];
+        return sum;
+    }
+
+    static int CalculateProductOfTwoElementsInFifthColumn(int[,] array, int rowIndex1, int rowIndex2)
+    {
+        int columnIndex = array.GetLength(1) - 1; // Отримуємо індекс останнього стовпця
+        int product = array[rowIndex1, columnIndex] * array[rowIndex2, columnIndex];
+        return product;
+    }
+
+    static void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
