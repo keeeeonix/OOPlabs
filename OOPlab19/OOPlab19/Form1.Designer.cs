1ef0dc2f21fde5d71e925babf2af24cfbb4326da
yoliyko <59864721+keeeeonix@users.noreply.github.com>
2024-04-13 14:38:19 +0300

    Add files via upload

---
+++ OOPlab19/OOPlab19/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace OOPlab19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProcessButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ProcessButton
+            // 
+            this.ProcessButton.Location = new System.Drawing.Point(190, 156);
+            this.ProcessButton.Name = "ProcessButton";
+            this.ProcessButton.Size = new System.Drawing.Size(75, 23);
+            this.ProcessButton.TabIndex = 0;
+            this.ProcessButton.Text = "Process";
+            this.ProcessButton.UseVisualStyleBackColor = true;
+            this.ProcessButton.Click += new System.EventHandler(this.ProcessButton_Click);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(88, 50);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.InputTextBox.TabIndex = 1;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(88, 107);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.OutputTextBox.TabIndex = 2;
+            this.OutputTextBox.TextChanged += new System.EventHandler(this.OutputTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 356);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.ProcessButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
---
+++ OOPlab19/OOPlab19/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPlab19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OutputTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessButton_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text;
+            string output = ReplaceConsecutiveCharacters(input);
+            OutputTextBox.Text = output;
+        }
+
+        private string ReplaceConsecutiveCharacters(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            int count = 1;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    result.Append(input[i]);
+                    if (count > 1)
+                    {
+                        result.Append(count);
+                    }
+                    count = 1;
+                }
+            }
+
+            // Додати останній символ та кількість, якщо вони існують
+            result.Append(input[input.Length - 1]);
+            if (count > 1)
+            {
+                result.Append(count);
+            }
+
+            return result.ToString();
+        }
+    }
+}
