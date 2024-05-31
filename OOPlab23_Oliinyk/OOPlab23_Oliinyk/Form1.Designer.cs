11f5ab5b60cf17606401f271110cd2691b7485ee
yoliyko <59864721+keeeeonix@users.noreply.github.com>
2024-05-31 12:34:38 +0300

    Add files via upload

---
+++ OOPlab23_Oliinyk/OOPlab23_Oliinyk/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace OOPlab23_Oliinyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(12, 12);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 20);
+            this.textBoxA.TabIndex = 0;
+            this.textBoxA.Text = "1";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(12, 38);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 20);
+            this.textBoxB.TabIndex = 1;
+            this.textBoxB.Text = "1";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(12, 64);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(100, 20);
+            this.textBoxC.TabIndex = 2;
+            this.textBoxC.Text = "0";
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(12, 90);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(100, 23);
+            this.buttonDraw.TabIndex = 3;
+            this.buttonDraw.Text = "Малювати графік";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Name = "Form1";
+            this.Text = "Графік функції";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+
+
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Button buttonDraw;
+    }
+}
+
---
+++ OOPlab23_Oliinyk/OOPlab23_Oliinyk/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPlab23_Oliinyk
+{
+    public partial class Form1 : Form
+    {
+        private float[] X;
+        private float[] Y;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Зчитування коефіцієнтів з текстових полів
+                float a = float.Parse(textBoxA.Text);
+                float b = float.Parse(textBoxB.Text);
+                float c = float.Parse(textBoxC.Text);
+
+                // Обчислення точок
+                CalculatePoints(a, b, c);
+
+                // Перемалювати форму
+                this.Invalidate();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введіть правильні значення коефіцієнтів.");
+            }
+        }
+
+        private void CalculatePoints(float a, float b, float c)
+        {
+            int pointsCount = 1000;
+            X = new float[pointsCount];
+            Y = new float[pointsCount];
+
+            float tMin = -5f;
+            float tMax = 5f;
+            float step = (tMax - tMin) / pointsCount;
+
+            for (int i = 0; i < pointsCount; i++)
+            {
+                float t = tMin + i * step;
+                X[i] = a * t * t * (float)Math.Sin(t); // Змінити на вашу функцію
+                Y[i] = b * t * (float)Math.Pow(Math.Cos(t), 2) + c; // Змінити на вашу функцію
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGraph(e.Graphics);
+        }
+
+        private void DrawGraph(Graphics g)
+        {
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Pen pen = new Pen(Color.Blue, 2);
+
+            // Центр форми
+            int centerX = this.ClientSize.Width / 2;
+            int centerY = this.ClientSize.Height / 2;
+
+            // Масштабування графіка для кращого відображення
+            float scaleX = 20;
+            float scaleY = 20;
+
+            // Малюємо осі
+            g.DrawLine(Pens.Black, centerX, 0, centerX, this.ClientSize.Height); // вісь Y
+            g.DrawLine(Pens.Black, 0, centerY, this.ClientSize.Width, centerY); // вісь X
+
+            // Підписи осей
+            Font font = new Font("Arial", 10);
+            Brush brush = Brushes.Black;
+            g.DrawString("X", font, brush, this.ClientSize.Width - 20, centerY + 5);
+            g.DrawString("Y", font, brush, centerX + 5, 5);
+
+            // Значення на осях
+            for (int i = -5; i <= 5; i++)
+            {
+                g.DrawString(i.ToString(), font, brush, centerX + 5, centerY - i * scaleY);
+                g.DrawString(i.ToString(), font, brush, centerX + i * scaleX, centerY + 5);
+            }
+
+            // Малюємо точки графіка
+            if (X != null && Y != null && X.Length == Y.Length)
+            {
+                // Малюємо точки графіка
+                for (int i = 0; i < X.Length - 1; i++)
+                {
+                    float x1 = centerX + X[i] * scaleX;
+                    float y1 = centerY - Y[i] * scaleY;
+                    float x2 = centerX + X[i + 1] * scaleX;
+                    float y2 = centerY - Y[i + 1] * scaleY;
+
+                    g.DrawLine(pen, x1, y1, x2, y2);
+                }
+            }
+            pen.Dispose();
+        }        
+    }
+}
