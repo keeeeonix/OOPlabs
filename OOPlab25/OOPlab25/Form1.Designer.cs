27611cdd231858f08f93f449116379f684ff9e3a
yoliyko <59864721+keeeeonix@users.noreply.github.com>
2024-06-14 14:26:02 +0300

    Add files via upload

---
+++ OOPlab25/OOPlab25/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace OOPlab25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(452, 513);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+        private void InitializeCustomComponents()
+        {
+            // Labels
+            Label lblChildName = new Label { Text = "Child Name", Location = new System.Drawing.Point(10, 10) };
+            Label lblGuardianName = new Label { Text = "Guardian Name", Location = new System.Drawing.Point(10, 40) };
+            Label lblGuardianPhone = new Label { Text = "Guardian Phone", Location = new System.Drawing.Point(10, 70) };
+            Label lblBirthDate = new Label { Text = "Birth Date", Location = new System.Drawing.Point(10, 100) };
+
+            // TextBoxes
+            txtChildName = new TextBox { Location = new System.Drawing.Point(120, 10) };
+            txtGuardianName = new TextBox { Location = new System.Drawing.Point(120, 40) };
+            txtGuardianPhone = new TextBox { Location = new System.Drawing.Point(120, 70) };
+
+            // DateTimePicker
+            dtpBirthDate = new DateTimePicker { Location = new System.Drawing.Point(120, 100) };
+
+            // Buttons
+            btnAdd = new Button { Text = "Add", Location = new System.Drawing.Point(10, 140) };
+            btnAdd.Click += new EventHandler(btnAdd_Click);
+
+            // Age Report
+            Label lblAge = new Label { Text = "Age", Location = new System.Drawing.Point(10, 180) };
+            txtAge = new TextBox { Location = new System.Drawing.Point(120, 180) };
+            btnReportByAge = new Button { Text = "Report by Age", Location = new System.Drawing.Point(250, 180) };
+            btnReportByAge.Click += new EventHandler(btnReportByAge_Click);
+
+            // Guardian Report
+            Label lblReportGuardianName = new Label { Text = "Guardian Name", Location = new System.Drawing.Point(10, 220) };
+            txtReportGuardianName = new TextBox { Location = new System.Drawing.Point(120, 220) };
+            btnReportByGuardian = new Button { Text = "Report by Guardian", Location = new System.Drawing.Point(250, 220) };
+            btnReportByGuardian.Click += new EventHandler(btnReportByGuardian_Click);
+
+            // Search
+            Label lblSearch = new Label { Text = "Search Child", Location = new System.Drawing.Point(10, 260) };
+            txtSearch = new TextBox { Location = new System.Drawing.Point(120, 260) };
+            btnSearch = new Button { Text = "Search", Location = new System.Drawing.Point(250, 260) };
+            btnSearch.Click += new EventHandler(btnSearch_Click);
+
+            // ListBox for results
+            listBoxResults = new ListBox { Location = new System.Drawing.Point(10, 300), Size = new System.Drawing.Size(350, 150) };
+
+            // Add controls to form
+            this.Controls.Add(lblChildName);
+            this.Controls.Add(txtChildName);
+            this.Controls.Add(lblGuardianName);
+            this.Controls.Add(txtGuardianName);
+            this.Controls.Add(lblGuardianPhone);
+            this.Controls.Add(txtGuardianPhone);
+            this.Controls.Add(lblBirthDate);
+            this.Controls.Add(dtpBirthDate);
+            this.Controls.Add(btnAdd);
+            this.Controls.Add(lblAge);
+            this.Controls.Add(txtAge);
+            this.Controls.Add(btnReportByAge);
+            this.Controls.Add(lblReportGuardianName);
+            this.Controls.Add(txtReportGuardianName);
+            this.Controls.Add(btnReportByGuardian);
+            this.Controls.Add(lblSearch);
+            this.Controls.Add(txtSearch);
+            this.Controls.Add(btnSearch);
+            this.Controls.Add(listBoxResults);
+        }
+        #endregion
+
+        private OleDbConnection connection;
+        private TextBox txtChildName;
+        private TextBox txtGuardianName;
+        private TextBox txtGuardianPhone;
+        private DateTimePicker dtpBirthDate;
+        private Button btnAdd;
+        private TextBox txtAge;
+        private Button btnReportByAge;
+        private TextBox txtReportGuardianName;
+        private Button btnReportByGuardian;
+        private TextBox txtSearch;
+        private Button btnSearch;
+        private ListBox listBoxResults;
+    }
+}
+
---
+++ OOPlab25/OOPlab25/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OOPlab25.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.OleDb;
+using System.Diagnostics.Tracing;
+using System.Drawing.Printing;
+
+namespace OOPlab25
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\ivano\source\repos\OOPlab25\kindergarden.accdb;";
+            connection = new OleDbConnection(connectionString);
+        }
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(txtChildName.Text))
+            {
+                MessageBox.Show("Please enter child name.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtGuardianName.Text))
+            {
+                MessageBox.Show("Please enter guardian name.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtGuardianPhone.Text))
+            {
+                MessageBox.Show("Please enter guardian phone.");
+                return false;
+            }
+            return true;
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInput())
+            {
+                return; 
+            }
+            string childName = txtChildName.Text;
+            string guardianName = txtGuardianName.Text;
+            string guardianPhone = txtGuardianPhone.Text;
+            DateTime birthDate = dtpBirthDate.Value;
+
+            string query = "INSERT INTO garden (ChildName, GuardianName, GuardianPhone, BirthDate) VALUES (?, ?, ?, ?)";
+
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@ChildName", childName);
+                command.Parameters.AddWithValue("@GuardianName", guardianName);
+                command.Parameters.AddWithValue("@GuardianPhone", guardianPhone);
+                command.Parameters.AddWithValue("@BirthDate", birthDate);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Record added successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+        private void btnReportByAge_Click(object sender, EventArgs e)
+        {
+            listBoxResults.Items.Clear();
+            string query = "SELECT ChildName, GuardianName, GuardianPhone, BirthDate FROM garden WHERE YEAR(BirthDate) = ?";
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Year", DateTime.Now.Year - int.Parse(txtAge.Text));
+                connection.Open();
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        listBoxResults.Items.Add($"{reader["ChildName"]}, {reader["GuardianName"]}, {reader["GuardianPhone"]}, {reader["BirthDate"]}");
+                    }
+                }
+                connection.Close();
+            }
+        }
+        private void btnReportByGuardian_Click(object sender, EventArgs e)
+        {
+            listBoxResults.Items.Clear();
+            string query = "SELECT ChildName, BirthDate FROM garden WHERE GuardianName = ?";
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@GuardianName", txtReportGuardianName.Text);
+                connection.Open();
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        listBoxResults.Items.Add($"{reader["ChildName"]}, {reader["BirthDate"]}");
+                    }
+                }
+                connection.Close();
+            }
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            listBoxResults.Items.Clear();
+            string query = "SELECT ChildName, GuardianName, GuardianPhone, BirthDate FROM garden WHERE ChildName LIKE ?";
+            using (OleDbCommand command = new OleDbCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@ChildName", "%" + txtSearch.Text + "%");
+                connection.Open();
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        listBoxResults.Items.Add($"{reader["ChildName"]}, {reader["GuardianName"]}, {reader["GuardianPhone"]}, {reader["BirthDate"]}");
+                    }
+                }
+                connection.Close();
+            }
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
